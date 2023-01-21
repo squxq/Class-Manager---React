1948b48af1322d11a8228c93ed37e4ffa6630bdf
Francisco Tomás <camelocomsede1@gmail.com>
2023-01-21 19:33:43 +0000

    url updates

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../../components/Navbar/index.jsx'
-import HeroSection from '../../components/HeroSection/index.jsx'
-import styles from '../../style.js'
-import Stats from '../../components/Stats'
-import './index.css'
-import Benefit1 from '../../components/Benefits/Benefit-1'
-import Benefit2 from '../../components/Benefits/Benefit-2/index.jsx'
-import Testimonials from '../../components/Testimonials/index.jsx'
-import CTA from '../../components/CTA/index.jsx'
-import Footer from '../../components/Footer2/index.jsx'
-import axios from 'axios'
+import React, { useState, useEffect } from "react"
+import Navbar from "../../components/Navbar/index.jsx"
+import HeroSection from "../../components/HeroSection/index.jsx"
+import styles from "../../style.js"
+import Stats from "../../components/Stats"
+import "./index.css"
+import Benefit1 from "../../components/Benefits/Benefit-1"
+import Benefit2 from "../../components/Benefits/Benefit-2/index.jsx"
+import Testimonials from "../../components/Testimonials/index.jsx"
+import CTA from "../../components/CTA/index.jsx"
+import Footer from "../../components/Footer2/index.jsx"
+import axios from "axios"
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -19,58 +19,60 @@ const Home = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      await axios.get(`http://localhost:5000/`, { withCredentials: false })
+      await axios
+        .get(`https://admin-backend-i5xx.onrender.com/`, {
+          withCredentials: false,
+        })
         .then((res) => {
           setHomeData(res.data.success)
         })
         .catch((err) => console.log(err))
     }
-    
+
     fetchData()
   }, [])
 
   switch (homeData) {
-    case false: 
-      return ( <h1>Loading...</h1> )
+    case false:
+      return <h1>Loading...</h1>
     case true:
       return (
         <div className="bg-primary w-full">
           <div className={`${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>
-              <Navbar toggle={ toggle }/> 
+              <Navbar toggle={toggle} />
             </div>
           </div>
-    
+
           <div className={`bg-primary ${styles.flexStart} hero-height`}>
             <div className={`${styles.boxWidth}`}>
-              <HeroSection />   
+              <HeroSection />
               <Stats />
             </div>
           </div>
-    
+
           <div className={`bg-secondary ${styles.flexStart}`}>
             <div className={`${styles.boxWidthFeatures}`}>
               <Benefit1 />
               <Benefit2 />
             </div>
           </div>
-    
+
           <div className={`bg-secondary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
               <Testimonials />
               <CTA />
             </div>
           </div>
-    
+
           <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
               <Footer />
             </div>
           </div>
         </div>
-      );
+      )
   }
-  
 }
 
 export default Home
